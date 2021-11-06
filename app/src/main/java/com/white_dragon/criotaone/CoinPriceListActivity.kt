b714f66fed704d8adbcbd227519282c4ba99d766
ViaMay <vi-113@yandex.ru>
2021-11-06 22:18:35 +0500

    fixed

--- app/src/main/java/com/white_dragon/criotaone/CoinPriceListActivity.kt
+++ app/src/main/java/com/white_dragon/criotaone/CoinPriceListActivity.kt
@@ -4,23 +4,20 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.RecyclerView
 import com.white_dragon.criotaone.adapters.CoinInfoAdapter
-import com.white_dragon.criotaone.databinding.ActivityCoinPriceListBinding
 import com.white_dragon.criotaone.pojo.CoinPriceInfo
 
 class CoinPriceListActivity : AppCompatActivity() {
     private lateinit var viewModel: CoinViewModel
-    lateinit var binding: ActivityCoinPriceListBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_coin_price_list)
 
-        binding = ActivityCoinPriceListBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
         val adapter = CoinInfoAdapter(this)
-        binding.rvCoinPriceList.adapter = adapter
+        var rvCoinPriceList = findViewById<RecyclerView>(R.id.rvCoinPriceList)
+        rvCoinPriceList.adapter = adapter
         adapter.onCoinClickListener = object : CoinInfoAdapter.OnCoinClickListener {
             override fun onCoinClick(coinPriceInfo: CoinPriceInfo) {
                 val intent = CoinDetailActivity.newIntent(this@CoinPriceListActivity, coinPriceInfo.fromSymbol)
